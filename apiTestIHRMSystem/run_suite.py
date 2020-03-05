66ef54e88adeac22857476c82ad7997726e6b072
wd939310817 <61624835+wd939310817@users.noreply.github.com>
2020-03-05 11:36:49 +0800

    Update run_suite.py

--- apiTestIHRMSystem/run_suite.py
+++ apiTestIHRMSystem/run_suite.py
@@ -10,7 +10,7 @@ from apiTestIHRMSystem.script.test_login_param import TestLogin
 suite = unittest.TestSuite()
 suite.addTest(unittest.makeSuite(TestLogin))
 suite.addTest(unittest.makeSuite(TestEmployee))
-report_path = os.path.dirname(os.path.abspath(__file__))+"/report/IHRM{}.html".format(time.strftime("%Y%m%d%H%M%S"))
+report_path = os.path.dirname(os.path.abspath(__file__))+"/report/IHRM.html"
 with open(report_path,mode='wb')as f:
     runner = HTMLTestRunner(f,verbosity=2,title="IHRM接口测试(登录和员工管理模块)",description="HTMLTestRunner_py3生成报告")
     runner.run(suite)
